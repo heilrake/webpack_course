b436df688415c9bcbbfff7a7b0a04921d928be5c
hailrake <mishakvochko@ukr.net>
2023-11-14 22:52:42 +0200

    21.28

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].[contenthash].js',
-    clean: true,
-  },
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+  return {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].[contenthash].js',
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+      new webpack.ProgressPlugin(),
+    ],
+  };
 };
