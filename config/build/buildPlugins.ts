914272f3a032458bc56902dc1d519650b1c4245d
hailrake <mishakvochko@ukr.net>
2024-01-14 14:00:45 +0200

    decompose webpack config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,32 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack, { Configuration } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/types";
+
+export function buildPlugins(options: BuildOptions): Configuration["plugins"] {
+  const { mode, paths } = options;
+
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+
+  const plugins: Configuration["plugins"] = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.ProgressPlugin()); // может замедлять зборку на больших проектах
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+      }),
+    );
+  }
+
+  return plugins;
+}
