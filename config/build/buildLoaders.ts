a4d0bf6e3e1839c0f5a05031931470c8893049ff
hailrake <mishakvochko@ukr.net>
2024-01-14 22:10:51 +0200

    adds assest & svg loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -30,11 +30,39 @@ export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
       "sass-loader",
     ],
   };
+
+  const assetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+  };
+
+  const svgrLoader = {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: "@svgr/webpack",
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: "convertColors",
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
   const tsLoader = {
     test: /\.tsx?$/,
     use: ["ts-loader"],
     exclude: /node_modules/,
   };
 
-  return [scssLoader, tsLoader];
+  return [scssLoader, tsLoader, assetLoader, svgrLoader];
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
-import classes from "./app.module.scss";
 import { Link, Outlet } from "react-router-dom";
+
+import classes from "./app.module.scss";
+import photo from "@/assets/photo.jpg";
+import Cloud from "@/assets/cloud.svg";
 export const App = () => {
   return (
     <div>
       <Link to={"/about"}>About</Link>
       <br />
       <Link to={"shop"}>Shop</Link>
+      <Cloud width={60} height={60} />
       Hello world!!
+      <img width={60} height={60} src={photo} alt="avatar" />
       <button className={classes.button}>Touch</button>
       <Outlet />
     </div>
