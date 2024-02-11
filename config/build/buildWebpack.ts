432fae58edf94f31e844ee2995b439eb4f19c37b
hailrake <mishakvochko@ukr.net>
2024-02-11 10:19:45 +0200

    change source map

--- config/build/buildWebpack.ts
+++ config/build/buildWebpack.ts
@@ -1,21 +1,21 @@
-import webpack from "webpack";
+import webpack from 'webpack';
 
-import { buildDevServer } from "./buildDevServer";
-import { buildLoaders } from "./buildLoaders";
-import { buildPlugins } from "./buildPlugins";
-import { buildResolvers } from "./buildResolvers";
-import { BuildOptions } from "./types/types";
+import { buildDevServer } from './buildDevServer';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { BuildOptions } from './types/types';
 
 export function buildWebpack(options: BuildOptions): webpack.Configuration {
   const { mode, paths } = options;
-  const isDev = mode === "development";
+  const isDev = mode === 'development';
 
   return {
-    mode: mode ?? "development",
+    mode: mode ?? 'development',
     entry: paths.entry,
     output: {
       path: paths.output,
-      filename: "[name].[contenthash].js",
+      filename: '[name].[contenthash].js',
       clean: true,
     },
     plugins: buildPlugins(options),
@@ -23,7 +23,7 @@ export function buildWebpack(options: BuildOptions): webpack.Configuration {
       rules: buildLoaders(options),
     },
     resolve: buildResolvers(options),
-    devtool: isDev && "inline-source-map",
+    devtool: isDev ? 'inline-source-map' : 'source-map',
     devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
